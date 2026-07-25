415dfd21b69c7582d5e8cd8382ba25cf0f1e019f
shivkumarmandal7368-del <shivkumarmandal7368@gmail.com>
2026-07-25 07:38:48 +0000

    gmail_uc_checker.py

--- artifacts/api-server/gmail_uc_checker.py
+++ artifacts/api-server/gmail_uc_checker.py
@@ -1223,7 +1223,7 @@ def make_stealth_js(fp: dict) -> str:
     except Exception:
         _tz_offset = 300   # CDT fallback
     return f"""
-(function(){{var _nts=Function.prototype.toString,_ntm=new WeakMap();Function.prototype.toString=function(){{return _ntm.has(this)?_ntm.get(this):_nts.call(this);}};_ntm.set(Function.prototype.toString,'function toString() {{ [native code] }}');Object.defineProperty(window,'__nr',{{value:function(fn,n,g){{if(fn&&typeof fn==='function')_ntm.set(fn,'function '+(g?'get ':'')+n+'() {{ [native code] }}');}},configurable:true,enumerable:false,writable:false}});}})();
+(function(){{var _nts=Function.prototype.toString,_ntm=new WeakMap();Function.prototype.toString=function toString(){{return _ntm.has(this)?_ntm.get(this):_nts.call(this);}};_ntm.set(Function.prototype.toString,'function toString() {{ [native code] }}');Object.defineProperty(window,'__nr',{{value:function(fn,n,g){{if(fn&&typeof fn==='function')_ntm.set(fn,'function '+(g?'get ':'')+n+'() {{ [native code] }}');}},configurable:true,enumerable:false,writable:false}});}})();
 (function(){{
   // Override on Navigator.prototype, not the navigator instance.
   // fingerprint.com's tampering detector calls Object.getOwnPropertyDescriptor on
